628555722d98c0f4f992a4254d8bf36351324ce6
yaaliuzhipeng <yaaliuzhipeng@outlook.com>
2023-08-10 18:04:16 +0800

    fix: remove MLKitTranslator to src

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 // main index.js
 
-export {default as MLKitTranslator} from './MLKitTranslator';
-export * from './MLKitTranslator';
+export {default as MLKitTranslator} from './src/MLKitTranslator';
+export * from './src/MLKitTranslator';
